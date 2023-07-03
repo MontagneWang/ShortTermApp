6b1ff2a4204d8f1386aa5b366513cf7c00e8c8e2
山遥路远 <2214981880@qq.com>
2023-07-03 14:43:08 +0800

    提交

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+Vue3 + TypeScript + Sass
+Vite + Vue-router + Pinia
+
+unplugin-auto-import 自动按需引入 api (vue/vue-router/pinia...)
+unplugin-vue-components 自动按需引入 第三方组件 和 自定义组件
+rollup-plugin-visualizer 打包后文件大小可视化
+
+--open 自动打开浏览器
+utils 引入
+normalize.css
+stores 引入
+axios 封装
+
+
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import {RouterLink, RouterView} from 'vue-router'
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+
+	<nav-bar msg="test"/>
+
+	<router-view v-slot="{ Component }">
+		<keep-alive>
+			<component :is="Component" v-if="$route.meta.keepAlive" :key="$route.name"/>
+		</keep-alive>
+		<component :is="Component" v-if="!$route.meta.keepAlive" :key="$route.name"/>
+	</router-view>
+
+</template>
+
+<style lang="scss">
+html *
+{
+	font-family: LXGW WenKai Lite, serif !important;
+}
+
+@media (min-width: 1024px) {
+}
+</style>
